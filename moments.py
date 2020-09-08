4aa889edca51eccd1e3243373cec8b1b3be96238
EsbenHygum <esbenhl1@gmail.com>
2020-09-08 11:22:31 +0200

    Update

--- moments.py
+++ moments.py
@@ -9,8 +9,7 @@ def r_M(alpha):
   r = (((4*A**2 - 5*A + 1) * (alpha + 3) * zeta(alpha + 2) * zeta(alpha + 4)) / ((2*A - 1)**2 * (alpha + 2) * (zeta(alpha + 3))**2))
   return r
 
-def bisection(f, a, b, N):
-  print(f(a), f(b))
+def bisection(f, a, b, N, x_abs_tol, epsilon):
   if f(a)*f(b) >= 0:
     print('Bisection method fails - 1')
     return None
@@ -19,19 +18,18 @@ def bisection(f, a, b, N):
   for n in range(1, N+1):
     m_n = (a_n + b_n)/2
     f_m_n = f(m_n)
-    if f(a_n)*f_m_n < 0:
-      a_n = a_n
-      b_n = m_n
-    elif f(b_n)*f_m_n < 0:
-      a_n = m_n
-      b_n = b_n
-    elif f_m_n == 0:
+    if f_m_n == 0:
       print('Found exact solution')
       return m_n
+    elif f(a_n)*f_m_n < 0:
+      b_n = m_n
     else:
-      print('Bisection method fails - 2')
-      return None
-  return (a_n + b_n)/2
+      a_n = m_n
+    if np.abs(a_n - b_n) < x_abs_tol:
+      return (a_n + b_n)/2
+    if np.abs((f(a_n) - f(b_n))/f_m_n) < epsilon:
+      return (a_n + b_n)/2
+  return None
 
 def get_alpha(r):
   a = -1
@@ -40,29 +38,19 @@ def get_alpha(r):
   i = 0
   j = 0
   f = lambda alpha: r_M(alpha) - r
-  while r > f(left):
+  while f(left) < 0:
     i += 1
     right = left
     left = -2 + 10**(-i)
-  while r < f(right):
+  while f(right) > 0:
     j += 1
     left = right
     right = 10**(j)
-  N = 100
-  alpha = bisection(f, left, right, N)
+  N = 1000
+  tol = 1e-14
+  epsilon = 1e-14
+  alpha = bisection(f, left, right, N, tol, epsilon)
   return alpha
 
-d = get_alpha(2)
+d = get_alpha(1.1)
 print(d)
-
-#r = 1.1
-#alpha = np.linspace(-1.9, 50, 100)
-#print(alpha[0])
-#f = [0] * len(alpha)
-#for ii in range(len(alpha)):
-#  f[ii] = r_M(alpha[ii]) - r
-
-#plt.plot(alpha, f)
-#plt.grid()
-#plt.show()
-
